818d1d474236e24a8cdfbd6c6440cb9ae629c737
HwangBo94 <89343149+HwangBo94@users.noreply.github.com>
2021-10-13 10:41:36 +0800

    test

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="秋萍" &&  pwd=="1234"){
+    if(userName=="生日快乐" &&  pwd=="tiantiankaixin"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
